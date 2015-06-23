88f53a0a1377c435ca414719951d6c94b79b320c
Michal J <michal.jaciubek@gmail.com>
2015-06-23 20:51:12 +0200

    js navigation added

---
+++ js/main.js
@@ -0,0 +1,18 @@
+document.getElementById("main-nav").innerHTML =
+"<h2 class='navHeader'><a href='index.html'>Portfolio</a></h2>" +
+"<ul id='menu'>" +
+	"<li><a href='images.html'>Images</a></li>" +
+	"<li><a href='posters.html'>Posters</a></li>" +
+	"<li><a href='blog.html'>Blog</a></li>" +
+	"<li><a href='about.html'>About</a></li>" +
+	"<li><a href='contact.html'>Contact</a></li>" +
+"</ul>" +
+"<div class='navBottom'>" +
+	"<div class='socialIcons'>" +
+		"<a href='#'><img src='img/facebook32.png'></a>" +
+		"<a href='#'><img src='img/flickr32.png'></a>" +
+		"<a href='#'><img src='img/instagram32.png'></a>" +
+		"<a href='#'><img src='img/twitter32.png'></a>" +
+	"</div>" +
+	"<div class='copyright'>Michał Jaciubek © 2015<br/>All rights reserved</div>" +
+"</div>";
